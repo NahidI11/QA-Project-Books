288ceb3a3cfbfae9878c983c40e77ce6ee4124d8
NahidI11 <111808023+NahidI11@users.noreply.github.com>
2022-10-29 00:57:08 +0100

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -2,4 +2,21 @@
 
 cd /home/jenkins/.jenkins/workspace/qa-books
 
-sudo docker stop myapp; sudo docker rm myapp; sudo docker rmi myapp
+def imageName = "myapp"
+env.imageName = "${imageName}"
+  def oldImageID = sh( 
+    script: 'sudo docker images -qf reference=\${imageName}:\${imageTag}',
+    returnStdout: true
+                     )
+
+echo "Image Name: " + "${imageName}"
+echo "Old Image: ${oldImageID}"
+
+if ( "${oldImageID}" != '' ) {
+    echo "Deleting image id: ${oldImageID}..."
+    sh "sudo docker stop ${oldImageID}"
+    sh "sudo docker rm ${oldImageID}"
+    sh "sudo docker rmi -f ${oldImageID}"
+      } else {
+        echo "No image to delete..."
+      }
