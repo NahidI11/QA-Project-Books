4ae542478bd6151332784bda301ee51149fa0d39
NahidI11 <111808023+NahidI11@users.noreply.github.com>
2022-10-30 22:00:34 +0000

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker ps -q --filter "name=myapp" | grep -q . && docker stop myapp && docker rm -fv myapp
+cd /home/jenkins/.jenkins/workspace/qa-books
+
+sudo docker stop myapp; sudo docker rm myapp; sudo docker rmi myapp
